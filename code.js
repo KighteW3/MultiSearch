982a1be40491f9be009ee2b21481dc9fa7bc7982
Kighte <kighte04@gmail.com>
2022-05-27 09:24:07 +0200

    NextSearch solution

--- code.js
+++ code.js
@@ -11,59 +11,97 @@ function noNext() {
     document.querySelector(".next-search-container").style.display = "none";
 }
 
-
-let brave = getRandomNumber(1,4);
-let dDG = getRandomNumber(1,4);
-let qWant = getRandomNumber(1,4);
-let metaG = getRandomNumber(1,4);
-let randomizeS = getRandomNumber(1,4);
-
-console.log(brave, dDG, qWant, metaG, randomizeS);
-
-if (brave == randomizeS) {
-    let braveN = "Brave";
-    document.querySelector(".next-search").innerHTML = `Your next search will be redirected to: ${braveN}`;
-}else if (randomizeS == dDG) {
-    let dDGN = "Duckduckgo";
-    document.querySelector(".next-search").innerHTML = `Your next search will be redirected to: ${dDGN}`;
-}else if (randomizeS == qWant) {
-    let qWantN = "Qwant";
-    document.querySelector(".next-search").innerHTML = `Your next search will be redirected to: ${qWantN}`;
-}else if (randomizeS == metaG) {
-    let metaGN = "Metager";
-    document.querySelector(".next-search").innerHTML = `Your next search will be redirected to: ${metaGN}`;
-}else {
-    noNext();
+function letRandomize(){
+    brave = getRandomNumber(1,4);
+    dDG = getRandomNumber(1,4);
+    qWant = getRandomNumber(1,4);
+    metaG = getRandomNumber(1,4);
+    randomizeS = getRandomNumber(1,4);
 }
+
+function letRandomizeIt() {
+    letRandomize();
+    searchObject = [brave, dDG, qWant, metaG, randomizeS];
+}
+
+function randomizeAgain() {
+    letRandomize();
+    return searchObject2 = [brave, dDG, qWant, metaG, randomizeS];
+}
+
+function randomizeValidate() {
+    letRandomizeIt();
+    if (searchObject[0] == searchObject[4]){
+        return searchObject;
+    } else if (searchObject[1] == searchObject[4]) {
+        return searchObject;
+    } else if (searchObject[2] == searchObject[4]){
+        return searchObject;
+    } else if (searchObject[3] == searchObject[4]) {
+        return searchObject;
+    } else {
+        randomizeValidate();
+    }
+}
+
+/* function randomizeValidate2(){
+    randomizeValidate();
+    searchObject;
+} */
+
+randomizeValidate();
+
+/* function randomizeBucle(){
+    for (i=0;i<10;i++) {
+        randomizeAgain();
+        if (searchObject[0] == searchObject[4]) {
+            return randomizeAgain;
+        }else if (searchObject[4] == searchObject[1]) {
+            return randomizeAgain;
+        }else if (searchObject[4] == searchObject[2]) {
+            return randomizeAgain;
+        }else if (searchObject[4] == searchObject[3]) {
+            return randomizeAgain;
+        }
+    }
+} */
+
+/* letRandomizeIt(); */
+
+/* console.log(brave, dDG, qWant, metaG, randomizeS); */
+
+function nextSearch() {
+    if (searchObject[0] == searchObject[4]) {
+        let braveN = "Brave";
+        document.querySelector(".next-search").innerHTML = `Your next search will be redirected to: ${braveN}`;
+    }else if (searchObject[4] == searchObject[1]) {
+        let dDGN = "Duckduckgo";
+        document.querySelector(".next-search").innerHTML = `Your next search will be redirected to: ${dDGN}`;
+    }else if (searchObject[4] == searchObject[2]) {
+        let qWantN = "Qwant";
+        document.querySelector(".next-search").innerHTML = `Your next search will be redirected to: ${qWantN}`;
+    }else if (searchObject[4] == searchObject[3]) {
+        let metaGN = "Metager";
+        document.querySelector(".next-search").innerHTML = `Your next search will be redirected to: ${metaGN}`;
+    }else {
+        randomizeBucle();
+    }
+}
+
+nextSearch();
 
 
 function formSubmit(){
     let content = document.getElementById("searchable").value;
-    if (brave == randomizeS) {
+    if (searchObject[0] == searchObject[4]) {
         window.open(`https://search.brave.com/search?q=${content}`);
-    }else if (randomizeS == dDG) {
+    }else if (searchObject[4] == searchObject[1]) {
         window.open(`https://duckduckgo.com/?q=${content}`);
-    }else if (randomizeS == qWant) {
+    }else if (searchObject[4] == searchObject[2]) {
         window.open(`https://www.qwant.com/?q=${content}`);
-    }else if (randomizeS == metaG) {
+    }else if (searchObject[4] == searchObject[3]) {
         window.open(`https://metager.es/meta/meta.ger3?eingabe=${content}&focus=web&s=&f=&ff=&ft=&m=`);
     }else{
-        let brave = getRandomNumber(1,4);
-        let dDG = getRandomNumber(1,4);
-        let qWant = getRandomNumber(1,4);
-        let metaG = getRandomNumber(1,4);
-        let randomizeS = getRandomNumber(1,4);
-        if (brave == randomizeS) {
-            window.open(`https://search.brave.com/search?q=${content}`);
-        }else if (randomizeS == dDG) {
-            window.open(`https://duckduckgo.com/?q=${content}`);
-        }else if (randomizeS == qWant) {
-            window.open(`https://www.qwant.com/?q=${content}`);
-        }else if (randomizeS == metaG) {
-            window.open(`https://metager.es/meta/meta.ger3?eingabe=${content}&focus=web&s=&f=&ff=&ft=&m=`);
-        }else{
-            return formSubmit();
-        }
+        randomizeBucle();
     }
 }
-
